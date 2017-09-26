28c5a99765f87bd92b20c8fd6775525370ff88e5
Павел Муркин <kot_tdf@mail.ru>
2017-09-26 21:19:33 +0300

    update layout board

--- assets/js/board.js
+++ assets/js/board.js
@@ -7,9 +7,18 @@ let source   = document.getElementById('board-template').innerHTML;
 let template = Handlebars.compile(source);
 let html     = template(store.state);
 
+let audioOpenAnswer  = document.getElementById('audio-open-answer');
+let audioWrongAnswer = document.getElementById('audio-wrong-answer');
+
 App.innerHTML = html;
 
 ipcRenderer.on('asynchronous-reply', (event, state) => {
-    html          = template(state);
-    App.innerHTML = html;
+    console.log(event);
+
+    let a1 = document.getElementById('answer-0');
+    a1.className += ' hover';
+    audioOpenAnswer.play();
+
+    // html          = template(state);
+    // App.innerHTML = html;
 });
